0b09f0a04c2384f450207acd2926212167189709
sadmagrage <95193077+sadmagrage@users.noreply.github.com>
2023-07-15 02:52:30 -0300

    cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require("cors");
+
 const comidaRouter = require("./routes/comidaRoute");
 const progressRouter = require("./routes/progressRoute");
 
 const app = express();
+
+app.use(cors({ origin: "https://sadmagrage.github.io" }));
 
 app.use(comidaRouter.route);
 app.use(progressRouter.route);
